ca76a6b20259996cdccf365b9fa5fcb6c7488ecb
JOANNABUUMA <nabuumaj6@gmail.com>
2024-04-22 02:28:32 +0100

    FUNDAMENTALS VARIABLES

--- src/declaration.js
+++ src/declaration.js
@@ -4,12 +4,14 @@
 // TODO: 1. Declare the variables firstName and age so that the tests pass
 
 // do not edit below this line
+const firstName = 'Jane'
 let firstNameExport = ''
 try {
   /* eslint-disable no-undef */
   firstNameExport = firstName
 } catch (e) {}
 
+const age = 35
 let ageExport = 0
 try {
   /* eslint-disable no-undef */
